8d9e72eab30c3f652f8f4a1f6c9393c5c84355c5
Samuel Whittenberger <sbw98978@gmail.com>
2024-03-13 22:32:05 -0400

    enable/disable

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,27 @@
-// chrome.runtime.onMessage.addListener(
-//   function(msg) {
-//
-//   }
-// );
+var enabled = true;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const powerIcon = document.getElementById("powerIcon");
+  console.log(powerIcon);
+  powerIcon.addEventListener("click", (e) => {
+    // TODO: need to store enabled value, gets reset to true every time extension popup reopens
+    // enabled = !enabled;
+    // sendMessage({ enabled: enabled });
+    if (!enabled) document.getElementById("powerIconPath").setAttribute("fill", "gray");
+    else document.getElementById("powerIconPath").setAttribute("fill", "blue");
+  });
+});
+
+function sendMessage(msg) {
+  getCurrentTab((tab) => {
+    chrome.tabs.sendMessage(tab.id, msg);
+  });
+}
+
+function getCurrentTab(callback) {
+  let queryOptions = { active: true };
+  chrome.tabs.query(queryOptions, ([tab]) => {
+    if (chrome.runtime.lastError) console.error(chrome.runtime.lastError);
+    callback(tab);
+  });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,27 @@
 console.log("content script loaded");
 
-// working receive message from chrome.tabs.sendMessage
-// chrome.runtime.onMessage.addListener(
-//   function(message, sender, sendResponse) {
-//     console.log("MESSAGE: ", message);
-//     console.log(sender);
-//     console.log(sendResponse);
-//     sendMessageToExtension();
-//   }
-// );
-
+var enabled = true;
 var ctrl = false;
+
+// working receive message from chrome.tabs.sendMessage
+chrome.runtime.onMessage.addListener(
+  function(message, sender, sendResponse) {
+    console.log(message)
+    if (message.enabled !== undefined) enabled = message.enabled;
+    console.log("enabled", enabled);
+  }
+);
 
 document.addEventListener("keyup", (e) => {
   if (e.code === "ControlLeft") ctrl = false;
 });
 
 document.addEventListener("keydown", (e) => {
+  if (!enabled) return;
   if (e.code === "ControlLeft") ctrl = true;
-  if (ctrl && e.code === "ArrowLeft") sendMessageToExtension({ action: "tab-left" });
-  if (ctrl && e.code === "ArrowRight") sendMessageToExtension({ action: "tab-right" });
-  if (ctrl && e.code === "ArrowUp") sendMessageToExtension({ action: "duplicate" });
+  else if (ctrl && e.code === "ArrowLeft") sendMessageToExtension({ action: "tab-left" });
+  else if (ctrl && e.code === "ArrowRight") sendMessageToExtension({ action: "tab-right" });
+  else if (ctrl && e.code === "ArrowUp") sendMessageToExtension({ action: "duplicate" });
 });
 
 function sendMessageToExtension(msg) {
