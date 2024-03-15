d4509583f0924f83e145381d1ddd879fde9a26cc
Samuel Whittenberger <sbw98978@gmail.com>
2024-03-14 22:22:43 -0400

    change modifiers

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,29 +1,31 @@
-var ctrl = false;
-var alt = false;
-var shift = false;
+var modifiers = {
+  leftCtrl: false,
+  leftAlt: false,
+  leftShift: false
+};
 
 // working receive message from chrome.tabs.sendMessage
 chrome.runtime.onMessage.addListener(
-  function(message, sender, sendResponse) {
+  function (message, sender, sendResponse) {
     console.log(message)
   }
 );
 
 document.addEventListener("keyup", (e) => {
-  if (e.code === "ControlLeft") ctrl = false;
-  else if (e.code === "AltLeft") alt = false;
-  else if (e.code === "ShiftLeft") shift = false;
+  if (e.code === "ControlLeft") modifiers.leftCtrl = false;
+  else if (e.code === "AltLeft") modifiers.leftAlt = false;
+  else if (e.code === "ShiftLeft") modifiers.leftShift = false;
 });
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "ControlLeft") ctrl = true;
-  else if (e.code === "AltLeft") alt = true;
-  else if (e.code === "ShiftLeft") shift = true;
-  else if (ctrl && e.code === "ArrowLeft") sendMessageToExtension({ action: "tab-left" });
-  else if (ctrl && e.code === "ArrowRight") sendMessageToExtension({ action: "tab-right" });
-  else if (ctrl && e.code === "ArrowUp") sendMessageToExtension({ action: "duplicate" });
-  else if (ctrl && e.code === "ArrowDown") sendMessageToExtension({ action: "promote" });
-  else if (alt && shift && e.code === "ArrowLeft") sendMessageToExtension({ action: "open-last" });
+  if (e.code === "ControlLeft") modifiers.leftCtrl = true;
+  else if (e.code === "AltLeft") modifiers.leftAlt = true;
+  else if (e.code === "ShiftLeft") modifiers.leftShift = true;
+  else if (modifiers.leftCtrl && e.code === "ArrowLeft") sendMessageToExtension({ action: "tab-left" });
+  else if (modifiers.leftCtrl && e.code === "ArrowRight") sendMessageToExtension({ action: "tab-right" });
+  else if (modifiers.leftCtrl && e.code === "ArrowUp") sendMessageToExtension({ action: "duplicate" });
+  else if (modifiers.leftCtrl && e.code === "ArrowDown") sendMessageToExtension({ action: "promote" });
+  else if (modifiers.leftAlt && modifiers.leftShift && e.code === "ArrowLeft") sendMessageToExtension({ action: "open-last" });
 });
 
 function sendMessageToExtension(msg: any) {
