894d265dbb02f79359abb5f914108042765c09c1
Samuel Whittenberger <sbw98978@gmail.com>
2024-03-21 21:13:52 -0400

    shortcuts menu

--- src/app/actions-list/actions-list.component.ts
+++ src/app/actions-list/actions-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { KeyNamePipe } from '../pipes/key-name.pipe';
 import { ActionNamePipe } from '../pipes/action-name.pipe';
 import { Shortcut } from '../../modules/types';
@@ -9,19 +9,45 @@ import { getShortcuts } from '../../modules/state';
   selector: 'app-actions-list',
   standalone: true,
   imports: [
+    NgIf,
     NgFor,
     KeyNamePipe,
     ActionNamePipe
+  ],
+  providers: [
+    KeyNamePipe
   ],
   templateUrl: './actions-list.component.html',
   styleUrl: './actions-list.component.scss'
 })
 export class ActionsListComponent {
+  constructor(private keyName: KeyNamePipe) {}
+
   shortcuts: Shortcut[] = [];
+  shortcutOpen = false;
+  currentShortcut: Shortcut | null = null;
 
   ngOnInit() {
     getShortcuts().then((val: Shortcut[]) => {
       this.shortcuts = val;
     });
+  }
+
+  openShortcut(i: number) {
+    this.shortcutOpen = true;
+    this.currentShortcut = this.shortcuts[i];
+  }
+
+  closeShortcut() {
+    this.shortcutOpen = false;
+  }
+
+  getSequence(seq: string[]) {
+    let ret = "";
+    for (let i = 0; i < seq.length; i++) {
+      ret += this.keyName.transform(seq[i]);
+      if (i < seq.length-1) ret += " + ";
+    }
+    return ret;
   }
 }
