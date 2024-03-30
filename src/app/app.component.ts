a2ca89cb4e6dddc4318fcbfc3cc03dd4c6c330b8
Samuel Whittenberger <sbw98978@gmail.com>
2024-03-29 22:51:07 -0400

    add warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { VersionService } from './services/version.service';
+import { Component } from "@angular/core";
+import { CommonModule, NgIf } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { VersionService } from "./services/version.service";
+import { getCurrentTab } from "../modules/actions";
+import { WarningComponent } from "./warning/warning.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [
     CommonModule,
     RouterOutlet,
-    NgIf
+    NgIf,
+    WarningComponent
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss"
 })
 export class AppComponent {
   constructor(private versionService: VersionService) {}
 
-  title = 'custom-actions';
+  title = "custom-actions";
+  warning = false;
   version = this.versionService.getVersion();
+
+  ngOnInit() {
+    getCurrentTab((t: chrome.tabs.Tab) => {
+      if (t.url?.startsWith("chrome://") || t.url?.startsWith("file://")) {
+        this.warning = true;
+      }
+    });
+  }
 }
